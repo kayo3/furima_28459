46c50a4e649557e06fc3864d35bfc1c0428baa3f
kayo3 <hogekayo@gmail.com>
2020-08-18 17:18:31 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # バリーデーションの設定
+  with_options presence: true do
+    validates :nickname
+    validates :password, :password_confirmation, confirmation: true,
+              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message:'Include both letters and numbers' }
+    validates :last_name, :first_name, 
+              format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message:'Full-width characters' }
+    validates :last_name_kana, :first_name_kana,
+              format: { with: /\A[ァ-ヶー－]+\z/, message:'Full-width katakana characters' }
+    validates :birthday
+  end
 end
