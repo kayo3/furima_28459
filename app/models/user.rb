ff583365c0fe31b5373429a9fb48202ff9e04702
kayo3 <hogekayo@gmail.com>
2020-08-18 19:47:15 +0900

    バリデーションの修正
    -正規表現の変数化
    -emailのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # バリーデーションの設定
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  KANA_REGEX = /\A[ァ-ヶー－]+\z/
   with_options presence: true do
     validates :nickname
-    validates :password, :password_confirmation, confirmation: true,
-              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message:'Include both letters and numbers' }
     validates :last_name, :first_name, 
-              format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message:'Full-width characters' }
+              format: { with: NAME_REGEX, message:'Full-width characters' }
     validates :last_name_kana, :first_name_kana,
-              format: { with: /\A[ァ-ヶー－]+\z/, message:'Full-width katakana characters' }
+              format: { with: KANA_REGEX, message:'Full-width katakana characters' }
     validates :birthday
   end
+  validates :email,
+              format: { with: EMAIL_REGEX, message:'Include @'}
+  validates :password, :password_confirmation, confirmation: true,
+            format: { with: PASSWORD_REGEX, message:'Include both letters and numbers' }
 end
