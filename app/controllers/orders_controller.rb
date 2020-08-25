85b5be40990cf849933476d53574b3bb04efdc5b
kayo3 <hogekayo@gmail.com>
2020-08-25 21:35:39 +0900

    ordersコントローラーの編集
    -ログインしていないユーザーは購入ページに遷移出来ない様にする
    （ログインページにリダイレクトされる）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,9 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 
   def create
