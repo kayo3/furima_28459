0f2f071603f680bf05dcde706496a2356ee2acca
kayo3 <hogekayo@gmail.com>
2020-08-26 15:52:14 +0900

    ordersコントローラーの編集
    -createアクションの定義
     エラーハンドリングの為の記述
    
    -find_itemとしてbefore_actionを定義
    -支払い処理の記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
+  before_action :find_item
+  
   def index
-    @item = Item.find(params[:item_id])
+    # @item = Item.find(params[:item_id])
     unless user_signed_in?
       redirect_to new_user_session_path
     end
@@ -8,8 +10,10 @@ class OrdersController < ApplicationController
 
   def create
     @perchase = OrderDelivery.new(perchase_params)
-    if @perchase.save
-      redirect_to root_path
+    if @perchase.valid?
+      pay_item 
+      @perchase.save
+      return redirect_to root_path
     else
       render :index
     end
@@ -19,5 +23,18 @@ private
 
   def perchase_params
     params.permit(:item_id, :postal_code, :prefecture_id, :city, :block, :building, :tel_number,:order_id).merge(user_id: current_user.id)
+  end
+  
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
   end
 end
