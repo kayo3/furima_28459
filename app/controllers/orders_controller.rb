d3558323b426e31306d16bc9357475ff5275d2fb
kayo3 <hogekayo@gmail.com>
2020-08-27 17:26:42 +0900

    ordersコントローラーの編集
    -indexアクションに変数を定義
    -createアクションの変数名を変更
    
    -paramsに@item.idをマージ
    
    -form_withのモデル設定により、tokenの記述を修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,17 +2,17 @@ class OrdersController < ApplicationController
   before_action :find_item
   
   def index
-    # @item = Item.find(params[:item_id])
+    @order_delivery = OrderDelivery.new
     unless user_signed_in?
       redirect_to new_user_session_path
     end
   end
 
   def create
-    @perchase = OrderDelivery.new(perchase_params)
-    if @perchase.valid?
+    @order_delivery = OrderDelivery.new(order_delivery_params)
+    if @order_delivery.valid?
       pay_item 
-      @perchase.save
+      @order_delivery.save
       return redirect_to root_path
     else
       render :index
@@ -21,8 +21,8 @@ class OrdersController < ApplicationController
 
 private
 
-  def perchase_params
-    params.permit(:item_id, :postal_code, :prefecture_id, :city, :block, :building, :tel_number,:order_id).merge(user_id: current_user.id)
+  def order_delivery_params
+    params.require(:order_delivery).permit(:token, :postal_code, :prefecture_id, :city, :block, :building, :tel_number,:order_id).merge(user_id: current_user.id, item_id: @item.id)
   end
   
   def find_item
@@ -30,11 +30,11 @@ private
   end
   
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]     # PAY.JPテスト秘密鍵
     Payjp::Charge.create(
-      amount: @item.price,  # 商品の値段
-      card: params[:token],    # カードトークン
-      currency:'jpy'                 # 通貨の種類(日本円)
+      amount: @item.price,                      # 商品の値段
+      card: params[:order_delivery][:token],    # カードトークン
+      currency:'jpy'                            # 通貨の種類(日本円)
     )
   end
 end
