616e25ff5b9cdc0a22f8cc3caad3b43cff2b6efd
kayo3 <hogekayo@gmail.com>
2020-09-03 01:41:37 +0900

    日本語翻訳用ファイルの作成
    -devise.ja.ymlの作成
    -ja.ymlの作成
     個別の日本語翻訳設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     password              {Faker::Internet.password(min_length: 6, mix_case: true)}
     password_confirmation {password}
     last_name             {"ふり間"}
-    first_name            {"太朗"}
+    first_name            {"太郎"}
     last_name_kana        {"フリマ"}
     first_name_kana       {"タロウ"}
     birthday              {Faker::Date.between(from: '1930-01-01', to: '2015-12-31') }
