7e80692b46fab511d377cbf2e439a947e2a16ea8
kayo3 <hogekayo@gmail.com>
2020-09-05 03:05:19 +0900

    プレビュー表示微修正
    -imgタグの親要素であるdivタグにclassの付与
    
    -cssにてプレビュー画像を中央配置にする記述

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,12 +1,12 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function(){
     const imageList = document.getElementById('image-list');
-    
+
     //選択した画像を表示する関数
     const createImageHTML = (blob) => {
       // 画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div');
-      
+      imageElement.className = 'uploaded_box';
       // 表示する画像を生成
       const blobImage = document.createElement('img');
       blobImage.className = 'uploaded_img';
